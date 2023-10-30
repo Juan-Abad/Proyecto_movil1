a7ed7bf250a2cefa77c3f3b6cf3ee90e6c504a4e
Juan <juan.abad.hernandez@gmail.com>
2023-10-30 19:37:42 +0100

    Creo app: ColorPalette

---
+++ app/src/main/java/com/juan/aplicacionespmdm/ColorPalette/ColorPalette.kt
@@ -0,0 +1,14 @@
+package com.juan.aplicacionespmdm.ColorPalette
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juan.aplicacionespmdm.R
+
+class ColorPalette : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_palette)
+    }
+
+
+}
